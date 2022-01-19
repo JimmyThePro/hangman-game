6b9424d582951ce99b0af82b16d3617272f1a81f
JimmyThePro <jimmyjunttila@hotmail.com>
2022-01-19 18:50:28 +0000

    User can now guess letters, but need to fix win_check, and also create a 'play again' loop

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import random
 from secret_words import secret_words
 
-class Hangman():
+class Hangman:
     """
     Main class.
     Has methods for displaying secret words,
@@ -12,21 +12,25 @@ class Hangman():
         self.word = random.choice(secret_words)
         # show underscore instead of the word for the user
         self.display = ['_' for letter in self.word]
-        self.guesses = 0
 
     # print display to terminal
     def view(self):
         display = ' '.join(self.display)
-        print(f'Secret word (an animal): {display}')
+        print(f'\nSecret word (an animal): {display}')
 
     # index/char value in secret words
     def get_letter_in_word(self, guess_letter):
         positions = []
         # learned code here: https://realpython.com/python-enumerate/
-        for index, char in enumerate(self.word):
+        for index, char in enumerate(list(self.word)):
             if char == guess_letter:
                 positions.append(index)
         return positions
+
+    # update display method
+    def display_update(self, index_value, letter):
+        for number in index_value:
+            self.display[number] = letter
 
     # check if our guessed letter is in the secret word
     def guess_check(self, guess_letter):
@@ -34,14 +38,9 @@ class Hangman():
             index_value = self.get_letter_in_word(guess_letter)
             self.display_update(index_value, guess_letter)
 
-    # update display method
-    def display_update(self, index_value, letter):
-        for letter in index_value:
-            self.display[number] = letter
-
-    # check if user win
-    def win_check(self, display):
-        display = ''.join(self)
+    # check if user win (need fix)
+    def win_check(self):
+        display = ''.join(self, display)
         word = self.word
         if display == word:
             print('Yay! You win!!')
@@ -49,9 +48,12 @@ class Hangman():
 
 def game():
     play = Hangman()
-    guess_letter = input('Guess a letter (a-z): ')
-    play.view()
-    play.guess_check(guess_letter)
-    
+    while True:
+        play.view()
+        guess = input('Guess a letter (a-z): ')
+        play.guess_check(guess)
+        if play.win_check() is True:
+            break
+
 
 game()

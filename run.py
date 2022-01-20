301ad5a4cadb7ca3ea77a4e44e8d864c40308459
JimmyThePro <jimmyjunttila@hotmail.com>
2022-01-20 13:01:22 +0000

    Fix that the game is no longer case-sensitive

--- run.py
+++ run.py
@@ -11,40 +11,40 @@ class Hangman:
 
     def __init__(self):
         self.word = random.choice(secret_words)
-        # show underscore instead of the word for the user
+        # Show underscore instead of the word for the user
         self.display = ['_' for letter in self.word]
 
-    # print display to terminal
+    # Print display to terminal
     def view(self):
-        display = ' '.join(self.display)
+        display = ' '.join(self.display).upper()
         print(f'\nSecret word (an animal): {display}')
 
-    # index/char value in secret words
+    # Index/char value in secret words
     def get_letter_in_word(self, guess_letter):
         positions = []
-        # learned code here: https://realpython.com/python-enumerate/
+        # Learned code here: https://realpython.com/python-enumerate/
         for index, char in enumerate(list(self.word)):
             if char == guess_letter:
                 positions.append(index)
         return positions
 
-    # check if our guessed letter is in the secret word
+    # Check if our guessed letter is in the secret word
     def guess_check(self, guess_letter):
         if guess_letter in self.word:
             index_value = self.get_letter_in_word(guess_letter)
             self.display_update(index_value, guess_letter)
 
-    # update display method
+    # Update display method
     def display_update(self, index_value, letter):
         for number in index_value:
             self.display[number] = letter
 
-    # check if user win
+    # Check if user win
     def win_check(self):
         display = ''.join(self.display)
         word = self.word
         if display == word:
-            print('Yay! You win!!')
+            print('\nYay, you did it!')
             return True
 
 
@@ -53,10 +53,10 @@ def game():
     print("\nLet's play Hangman! Good luck! :)")
     while True:
         play.view()
-        guess = input('Guess a letter (a-z): ')
+        guess = input('Guess a letter [a-z]: ').lower()
         play.guess_check(guess)
         if play.win_check():
-            print('U won!')
+            print('Great work! :)\n')
             break
 
 
@@ -65,13 +65,13 @@ game()
 
 def game_loop():
     while True:
-        response = input('Wanna play again? (Y/N): ')
+        response = input('Wanna play again? [y/n]: ').lower()
         if response == 'n':
             break
         elif response == 'y':
             game()
         else:
-            print('Invalid key - Please enter "Y" or "N" to continue')
+            print('Invalid key - Please enter "y" or "n" to continue')
 
 
 game_loop()
